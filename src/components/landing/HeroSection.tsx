bbdae2ec42b6715db9f65197c9c5df56c7fc1420
gandalfg <tasos.valtinos@gmail.com>
2026-08-06 14:31:41 +0200

    uvp final context hero

--- src/components/landing/HeroSection.tsx
+++ src/components/landing/HeroSection.tsx
@@ -18,7 +18,7 @@ export function HeroSection() {
              agents, fixing CI and
             </span>
             <span className="block whitespace-nowrap">
-            context switching all day?<span style={{ fontSize: '0.9em', marginLeft: '-0.1em' }}>😮‍💨</span>
+            switching context all day?<span style={{ fontSize: '0.9em', marginLeft: '-0.1em' }}>😮‍💨</span>
             {/* Try Coolplugz<span style={{ fontSize: '0.9em', marginLeft: '-0.2em' }}>😎</span> */}
             {/* <span style={{ fontSize: '1.0em', marginLeft: '-0.3em' }}>🔌</span> */}
             </span>
